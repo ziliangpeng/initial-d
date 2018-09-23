68e0dc23a021b0509e58f59d1d56791bcce03b6f
Victor Peng <victor.peng@airbnb.com>
2018-09-23 00:02:31 -0700

    simple /run endpoint to return request data

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Dark\n"
+
+
+@app.route('/run', methods=['POST'])
+def run():
+    return request.get_data()
 
 
 if __name__ == '__main__':
